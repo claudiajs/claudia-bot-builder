4a8a51873bb82132f375bc764499e40bcbcf39b7
simalexan <alexander.simovic@gmail.com>
2016-06-01 21:30:11 +0200

    adding proper recipient - chat_id, properly parsing

--- api.js
+++ api.js
@@ -10,7 +10,7 @@ botBuilder.onMessage(function (request) {
       sender: '',
       text: '',
       originalRequest: {},
-      type: '' // facebook, slackSlashCommand, slackBot
+      type: '' // facebook, slackSlashCommand, slackBot, telegram
     }
   */
   return Promise.resolve(`Why ${request.text} > ${excuse.get()}`)
