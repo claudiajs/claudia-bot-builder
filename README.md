110f4ff4b93848e4ecb4b0c59179db289bfa2079
Gojko Adzic <gojko@gojko.com>
2016-06-02 10:12:47 +0200

    initial jasmine tests

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Bot Builder
+
+
+## Running tests
+
+Run all
+
+```bash
+npm run test
+```
+
+Run only some tests
+
+```bash
+npm run test -- filter=prefix
+```
+
+get detailed name reporting
+
+```bash
+npm run test -- full
+```
+
---
+++ spec/support/jasmine-runner.js
@@ -0,0 +1,18 @@
+/*global jasmine, require, process*/
+var Jasmine = require('jasmine'),
+  SpecReporter = require('jasmine-spec-reporter'),
+  noop = function () {},
+  jrunner = new Jasmine(),
+  filter;
+process.argv.slice(2).forEach(function (option) {
+  'use strict';
+  if (option === 'full') {
+    jrunner.configureDefaultReporter({print: noop});    // remove default reporter logs
+    jasmine.getEnv().addReporter(new SpecReporter());   // add jasmine-spec-reporter
+  }
+  if (option.match('^filter=')) {
+    filter = option.match('^filter=(.*)')[1];
+  }
+});
+jrunner.loadConfigFile();                           // load jasmine.json configuration
+jrunner.execute(undefined, filter);
