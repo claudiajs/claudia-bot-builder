46323348c1a0f57ccd389696539a13c2a7f4120e
simalexan <alexander.simovic@gmail.com>
2016-09-02 00:09:17 +0200

    fixed previous Skype tests

--- spec/skype/skype-parse-spec.js
+++ spec/skype/skype-parse-spec.js
@@ -14,15 +14,15 @@ describe('Skype parse', () => {
   });
   it('returns false if the message content is missing', () => {
     expect(parse({from: {id: 111}, contextId: '2342342fwefwsdf'})).toBeUndefined();
-    expect(parse({content: undefined})).toBeUndefined();
+    expect(parse({text: undefined})).toBeUndefined();
   });
-  it('returns a parsed object with proper contextId when the content is present and contextId is present', () => {
-    var msg = {content:'Bonjour Skype', from: {id: 123412312}, id: 324234234};
+  it('returns a parsed object with proper contextId when the text is present and contextId is present', () => {
+    var msg = {text:'Bonjour Skype', from: {id: 123412312}, id: 324234234};
     var contextId = '3sdfsdfsdf24';
-    expect(parse(msg, contextId)).toEqual({ sender: {id: 123412312}, text: 'Bonjour Skype', originalRequest: 324234234, contextId: '3sdfsdfsdf24', type: 'skype'});
+    expect(parse(msg, contextId)).toEqual({ sender: 123412312, text: 'Bonjour Skype', originalRequest: msg, contextId: '3sdfsdfsdf24', type: 'skype'});
   });
   it('returns a parsed object with undefined contextId when the content is present and contextId is not', () => {
-    var msg = {content:'Bonjour Skype', from: {id: 123412312}, id: 324234234};
-    expect(parse(msg)).toEqual({ sender: {id: 123412312}, text: 'Bonjour Skype', originalRequest: 324234234, contextId: undefined, type: 'skype'});
+    var msg = {text:'Bonjour Skype', from: {id: 123412312}, id: 324234234};
+    expect(parse(msg)).toEqual({ sender: 123412312, text: 'Bonjour Skype', originalRequest: msg, contextId: undefined, type: 'skype'});
   });
 });
