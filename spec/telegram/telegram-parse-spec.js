53292e62d1decf843db9545d9df9dc330c84a59f
Jimmy Chan <jimmychan@morrosystems.com>
2016-09-13 13:11:08 -0700

    change the telegram-parse-spec
    change the telegram-parse-spec

--- spec/telegram/telegram-parse-spec.js
+++ spec/telegram/telegram-parse-spec.js
@@ -3,13 +3,9 @@
 var parse = require('../../lib/telegram/parse');
 
 describe('Telegram parse', () => {
-  it('returns nothing if the format is invalid', () => {
-    expect(parse('string')).toBeUndefined();
-    expect(parse()).toBeUndefined();
-    expect(parse(false)).toBeUndefined();
-    expect(parse(123)).toBeUndefined();
-    expect(parse({})).toBeUndefined();
-    expect(parse([1, 2, 3])).toBeUndefined();
+  it('returns original messageObject if format is not a message or inline_query to avoid failing silently', () => {
+    var msg = {callback_query: {id: 'some123CbId', {message: {chat: {id: 'some123ChatId'}, text: 'ello Telegram' }}}};
+    expect(parse(msg)).toEqual({originalRequest: msg, type: 'telegram'});
   });
   it('returns false the chat id are missing', () => {
     expect(parse({message: {chat: 'some123ChatId', text: 'ello Telegram'}})).toBeUndefined();
