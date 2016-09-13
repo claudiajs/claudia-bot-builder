61e6f15b37c407fcdd8175b15a756c233d25d8ee
Jimmy Chan <jimmychan@morrosystems.com>
2016-09-13 13:34:31 -0700

    fix telegram-parse-spec typo
    fix telegram-parse-spec typo

--- spec/telegram/telegram-parse-spec.js
+++ spec/telegram/telegram-parse-spec.js
@@ -4,7 +4,7 @@ var parse = require('../../lib/telegram/parse');
 
 describe('Telegram parse', () => {
   it('returns original messageObject if format is not a message or inline_query to avoid failing silently', () => {
-    var msg = {callback_query: {id: 'some123CbId', {message: {chat: {id: 'some123ChatId'}, text: 'ello Telegram' }}}};
+    var msg = {callback_query: {id: 'some123CbId', message: {chat: {id: 'some123ChatId'}, text: 'ello Telegram' }}};
     expect(parse(msg)).toEqual({originalRequest: msg, type: 'telegram'});
   });
   it('returns false the chat id are missing', () => {
