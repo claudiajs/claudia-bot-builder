b5a87a115c8ca2ead89d0d17b8ccdafe46cfb0dd
Slobodan Stojanovic <slobodan@cloudhorizon.com>
2016-10-27 16:34:22 +0200

    Fix skype test

--- spec/skype/skype-parse-spec.js
+++ spec/skype/skype-parse-spec.js
@@ -17,12 +17,12 @@ describe('Skype parse', () => {
     expect(parse({text: undefined})).toBeUndefined();
   });
   it('returns a parsed object with proper contextId when the text is present and contextId is present', () => {
-    var msg = {text:'Bonjour Skype', from: {id: 123412312}, id: 324234234};
+    var msg = {text:'Bonjour Skype', conversation: {id: 123412312}, id: 324234234};
     var contextId = '3sdfsdfsdf24';
     expect(parse(msg, contextId)).toEqual({ sender: 123412312, text: 'Bonjour Skype', originalRequest: msg, contextId: '3sdfsdfsdf24', type: 'skype'});
   });
   it('returns a parsed object with undefined contextId when the content is present and contextId is not', () => {
-    var msg = {text:'Bonjour Skype', from: {id: 123412312}, id: 324234234};
+    var msg = {text:'Bonjour Skype', conversation: {id: 123412312}, id: 324234234};
     expect(parse(msg)).toEqual({ sender: 123412312, text: 'Bonjour Skype', originalRequest: msg, contextId: undefined, type: 'skype'});
   });
 });
