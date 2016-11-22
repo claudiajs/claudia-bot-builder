a76f6bdc3516827b3f6ae4c270a01a79b004d2d2
Slobodan Stojanovic <slobodan@cloudhorizon.com>
2016-11-22 17:41:02 +0100

    Parse other requests from alexa

--- spec/alexa/alexa-parse-spec.js
+++ spec/alexa/alexa-parse-spec.js
@@ -4,7 +4,7 @@
 var parse = require('../../lib/alexa/parse');
 
 describe('Alexa parse', () => {
-  it('returns nothing if the format is invalid', () => {
+  it('should return nothing if the format is invalid', () => {
     expect(parse('string')).toBeUndefined();
     expect(parse()).toBeUndefined();
     expect(parse(false)).toBeUndefined();
@@ -12,17 +12,19 @@ describe('Alexa parse', () => {
     expect(parse({})).toBeUndefined();
     expect(parse([1, 2, 3])).toBeUndefined();
   });
-  it('returns undefined if the intent is missing', () => {
-    expect(parse({request: {}, session: { user: 'claudia test user'} })).toBeUndefined();
-  });
-  it('returns undefined if the session user is missing', () => {
+  it('should return undefined if the session user is missing', () => {
     expect(parse({request: { intent: { name: 'intent 1'}}, session: {} })).toBeUndefined();
   });
-  it('returns undefined if the intent name is missing', () => {
-    expect(parse({request: { intent: {}}, session: { user: 'claudia test user'} })).toBeUndefined();
+  it('should return original request with an empty text if the intent is missing', () => {
+    let msg = {request: {}, session: { user: { userId: 'claudia alexa user'} } };
+    expect(parse(msg)).toEqual({ sender: 'claudia alexa user', text: '', originalRequest: msg, type: 'alexa-skill'});
   });
-  it('returns a parsed object with proper sender and text when the intent name and session user are present', () => {
-    var msg = {
+  it('should return original request with an empty text if the intent name is missing', () => {
+    let msg = {request: { intent: {}}, session: { user: { userId: 'claudia alexa user'} } };
+    expect(parse(msg)).toEqual({ sender: 'claudia alexa user', text: '', originalRequest: msg, type: 'alexa-skill'});
+  });
+  it('should return a parsed object with proper sender and text when the intent name and session user are present', () => {
+    let msg = {
       request: { intent: { name: 'intent 1'}},
       session: { user: { userId: 'claudia alexa user'}} };
     expect(parse(msg)).toEqual({ sender: 'claudia alexa user', text: '', originalRequest: msg, type: 'alexa-skill'});
