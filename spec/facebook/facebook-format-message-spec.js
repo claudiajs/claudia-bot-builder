8ed79041232d9fc8bec41bbadfb73ca07b621a51
Ivan Jovanovic <ivanjov96@gmail.com>
2016-12-08 13:42:43 +0100

    Increased Facebook button text limit to 640 from official docs (#63)
    * Increased Facebook button text limit to 640 from official docs
    
    * Updated number in test

--- spec/facebook/facebook-format-message-spec.js
+++ spec/facebook/facebook-format-message-spec.js
@@ -351,8 +351,8 @@ describe('Facebook format message', () => {
       expect(() => new formatFbMessage.Button()).toThrowError('Button template text cannot be empty');
     });
 
-    it('should throw an error if button text is longer than 320 characters', () => {
-      expect(() => new formatFbMessage.Button(Array(321).fill('x').join(''))).toThrowError('Button template text cannot be longer than 320 characters');
+    it('should throw an error if button text is longer than 640 characters', () => {
+      expect(() => new formatFbMessage.Button(Array(641).fill('x').join(''))).toThrowError('Button template text cannot be longer than 640 characters');
     });
 
     it('should create a button template with the text when valid text is provided', () => {
