622926093e658afa576aa3445f89e949b81151d2
Slobodan Stojanovic <slobodan@cloudhorizon.com>
2017-01-03 12:17:31 +0100

    Fix parse for message echos

--- spec/facebook/facebook-parse-spec.js
+++ spec/facebook/facebook-parse-spec.js
@@ -80,15 +80,19 @@ describe('Facebook parse', () => {
   });
   it('does not parse the object if it is an echo message', () => {
     var msg = {
-      sender: { id: '12345' },
-      recipient: { id: '67890' },
-      timestamp: 1465558466933,
+      sender: {
+        id: '12345'
+      },
+      recipient: {
+        id: '54321'
+      },
+      timestamp: 1483413621558,
       message: {
         is_echo: true,
-        app_id: 1517776481860111,
-        metadata: 'DEVELOPER_DEFINED_METADATA_STRING',
-        mid: 'mid.1457764197618:41d102a3e1ae206a38',
-        seq: 73
+        app_id: 314159,
+        mid: 'mid.1483413621558:a9dc28cb84',
+        seq: 1022,
+        text: 'Some text'
       }
     };
     expect(parse(msg)).toBeFalsy();
