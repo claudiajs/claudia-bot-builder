7a2eb92bf31aa1ede0e5242ffe89277178657f25
Datrio <datrio@gmail.com>
2017-01-06 20:34:40 +0100

    Support commas in URLs (#76)
    * commas should also be supported in URLs
    
    * Update URL validation regex: commas should be available characters
    
    * Update URL validation regex: braces should also be available characters

--- spec/is-url-spec.js
+++ spec/is-url-spec.js
@@ -16,6 +16,7 @@ describe('isUrl', () => {
     expect(isUrl('https://www.claudiajs.com')).toBeTruthy();
     expect(isUrl('https://github.com/claudiajs')).toBeTruthy();
     expect(isUrl('https://github.com/claudiajs/claudia-bot-builder')).toBeTruthy();
+    expect(isUrl('https://www.google.com/#q=claudia,bot')).toBeTruthy();
   });
   it('should return false if url is in the sentence', () => {
     expect(isUrl('This is a valid url: http://google.com')).toBeFalsy();
