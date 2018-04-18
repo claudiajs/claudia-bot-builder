6dc2bec31b7a70968b4e872e610a9784013c5798
Bill Brinkert <william.brinkert@akqa.com>
2018-04-18 10:59:26 -0700

    feat: update facebook-reply-spec with tests to cover new reply formats

--- spec/facebook/facebook-reply-spec.js
+++ spec/facebook/facebook-reply-spec.js
@@ -1,11 +1,13 @@
 /*global describe, it, expect, require, jasmine, beforeEach */
 var reply = require('../../lib/facebook/reply'),
   https = require('https');
+
 describe('Facebook Reply', () => {
   'use strict';
   beforeEach(() =>{
     jasmine.DEFAULT_TIMEOUT_INTERVAL = 5000;
   });
+
   it('includes the token in the path request', done => {
     https.request.pipe(callOptions => {
       expect(callOptions).toEqual(jasmine.objectContaining({
@@ -20,6 +22,7 @@ describe('Facebook Reply', () => {
     });
     reply('user123', 'Hi there', 'ACCESS123');
   });
+
   it('sends string messages as a text object', done => {
     https.request.pipe(callOptions => {
       expect(JSON.parse(callOptions.body)).toEqual({
@@ -34,6 +37,7 @@ describe('Facebook Reply', () => {
     });
     reply('user123', 'Hi there', 'ACCESS123');
   });
+
   it('sends large text messages split into several', done => {
     var longLongMessage = new Array(201).join('blok ');
 
@@ -61,6 +65,7 @@ describe('Facebook Reply', () => {
       });
     }).then(done, done.fail);
   });
+
   it('sends requests in sequence', done => {
     var fiveHundred = new Array(101).join('blok ');
 
@@ -74,6 +79,7 @@ describe('Facebook Reply', () => {
 
     reply('user123', fiveHundred, 'ACCESS123');
   });
+
   describe('when an array is passed', () => {
     it('does not send the second request until the first one completes', done => {
       let answers = ['foo', 'bar'];
@@ -101,6 +107,7 @@ describe('Facebook Reply', () => {
     });
 
   });
+
   it('sends complex messages without transforming into a text object', done => {
     https.request.pipe(callOptions => {
       expect(JSON.parse(callOptions.body)).toEqual({
@@ -118,22 +125,27 @@ describe('Facebook Reply', () => {
     });
     reply('user123', {template: 'big', contents: { title: 'red'} }, 'ACCESS123');
   });
+
   it('does not send a message if message is not provided', () => {
     reply('user123', null, 'ACCESS123')
       .then(() => {
-        expect(https.request.calls.length).toBe(0);
-      });
+        expect(https.request.calls).toBeUndefined();
+      })
+      .catch(err => console.log(err));
 
     reply('user123', false, 'ACCESS123')
       .then(() => {
-        expect(https.request.calls.length).toBe(0);
-      });
+        expect(https.request.calls).toBeUndefined();
+      })
+      .catch(err => console.log(err));
 
     reply('user123', undefined, 'ACCESS123')
       .then(() => {
-        expect(https.request.calls.length).toBe(0);
-      });
+        expect(https.request.calls).toBeUndefined();
+      })
+      .catch(err => console.log(err));
   });
+
   it('sets a typing action if "sender_action" is passed', done => {
     https.request.pipe(callOptions => {
       expect(JSON.parse(callOptions.body)).toEqual({
@@ -146,14 +158,59 @@ describe('Facebook Reply', () => {
     });
     reply('user123', { sender_action: 'typing_on' }, 'ACCESS123');
   });
+
+  it('sets a notification type if "notification_type" is passed', done => {
+    https.request.pipe(callOptions => {
+      expect(JSON.parse(callOptions.body)).toEqual({
+        recipient: {
+          id: 'user123'
+        },
+        notification_type: 'SILENT',
+        message: {}
+      });
+      done();
+    });
+    reply('user123', { notification_type: 'SILENT' }, 'ACCESS123');
+  });
+
+  it('sets a messaging type if "messaging_type" is passed', done => {
+    https.request.pipe(callOptions => {
+      expect(JSON.parse(callOptions.body)).toEqual({
+        recipient: {
+          id: 'user123'
+        },
+        messaging_type: 'RESPONSE',
+        message: {}
+      });
+      done();
+    });
+    reply('user123', { messaging_type: 'RESPONSE' }, 'ACCESS123');
+  });
+
+  it('sets a message tag if "message_tag" is passed', done => {
+    https.request.pipe(callOptions => {
+      expect(JSON.parse(callOptions.body)).toEqual({
+        recipient: {
+          id: 'user123'
+        },
+        tag: 'ACCOUNT_UPDATE',
+        message: {}
+      });
+      done();
+    });
+    reply('user123', { message_tag: 'ACCOUNT_UPDATE' }, 'ACCESS123');
+  });
+
   it('does not resolve before the https endpoint responds', done => {
     https.request.pipe(done);
     reply('user123', {template: 'big', contents: { title: 'red'} }, 'ACCESS123').then(done.fail, done.fail);
   });
+
   it('resolves when the https endpoint responds with 200', done => {
     https.request.pipe(function () {
       this.respond('200', 'OK', 'Hi there');
     });
     reply('user123', {template: 'big', contents: { title: 'red'} }, 'ACCESS123').then(done, done.fail);
   });
+
 });
